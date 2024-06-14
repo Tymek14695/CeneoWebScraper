5fa248888aa1d736a38b95c5d5b78f10936933e8
Tymek14695 <14695@eans-nt.edu.pl>
2024-06-14 14:29:47 +0200

    fixed errors and made frontend beautiful

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from flask import render_template, request, redirect, url_for
 import requests
 import os
 from bs4 import BeautifulSoup
+from app import utils
+import json
 
 
 
@@ -31,7 +33,21 @@ def extract():
         if response.status_code == requests.codes['ok']:
             page = BeautifulSoup(response.text, 'html.parser')
             opinions_count = page.select_one("a.product-review__link > span")
-            if opinions_count:
+            all_opinions = []
+            for opinion in opinions:
+                single_opinion = {
+                    key: utils.get_data(opinion, *value)
+                        for key, value in utils.selectors.items()
+                }
+                all_opinions.append(single_opinion)
+                if not os.path.exists("app/data"):
+                    os.mkdir("app/data")
+                if not os.path.exists("app/data/opinions"):
+                    os.mkdir("app/data/opinions")
+                jf = open(f'app/data/opinions/{product_id}.json', 'w', encoding='UTF-8')
+                json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+                jf.close()
+                return redirect(url_for('product', product_id=product_id))
                 #ekstrakcja
                 report()
             return render_template('extract.html.jinja', error='Produkt o podanym kodzie nie posiada opinii.')
@@ -47,6 +63,11 @@ def products():
 def about():
     return render_template('about.html.jinja')
 
-@app.route('/products/<product_id>')
+@app.route('/product/<product_id>')
 def product(product_id):
+    if not os.path.exists('app/data/opinions'):
+        return redirect(url_for('extract'))
+    jf = open(f'app/data/opinions/{product_id}.json', 'r', encoding='UTF-8')
+    opinions = json.load(jf)
+    jf.close()
     return render_template('product.html.jinja', product_id=product_id)
